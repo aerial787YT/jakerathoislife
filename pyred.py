1f3091c95549b78d36f85e09d5835aabf92fed50
aerial787YT <161941050+aerial787YT@users.noreply.github.com>
2024-03-02 12:42:23 +0100

    Add files via upload

---
+++ pyred.py
@@ -0,0 +1,21 @@
+print("Jake Rathois Life")
+age = 20
+print(age)
+print("Thats his age!")
+price = 20
+print(price)
+print("Thats his net worth! Oops thats not it.... Eh i will fix it later who cares")
+is_online = False
+false = ("Hes poor.... Is it false or true? Check in the code file to see!")
+print(false)
+true = ("Hes rich! Is it true? Check the code file!")
+print("Did you guess?")
+print(true)
+print("Guess that!")
+print("www.youtube.com/@aerial787")
+print("Subcribe!")
+subcribe_like_share = ("Sub like and share this to anyone else who has Python!")
+print(subcribe_like_share)
+is_online = ("Now hes dead......:((((((((((( ")
+print(is_online)
+
